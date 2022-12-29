ee1ace558ce0121e2656720dcda5821de8f4c55c
Dong1333 <mogijisung13@gmail.com>
2022-12-29 19:04:31 +0900

    Connection Pool인 Hikari를 XML 설정, java 설정으로 각각 설정한 예제 작성

--- ex00/src/test/java/org/zerock/persistence/JDBCTests.java
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -11,7 +11,7 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class JDBCTests {
-	// 드라이버 연결
+	// 고정적으로 드라이버 로드 하기(항상 사용가능 / 공유의 개념)
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -24,6 +24,7 @@ public class JDBCTests {
 	@Test
 	public void testConnection(){
 		
+		// DB 접속 정보 (주소/DB이름, 유저 이름, 접속 비밀번호)
 		String sUrl = "jdbc:mysql://localhost:3306/firstDB";
 		String sUser = "root";
 		String sPwd = "12341234";
@@ -32,6 +33,7 @@ public class JDBCTests {
 		try(Connection con = 
 			DriverManager.getConnection(sUrl, sUser, sPwd))
 		{
+		// Log4j를 이용한 로그 정보 출력
 		log.info(con);
 		} 
 		catch (Exception e) {
--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,12 +1,34 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 
 // java 설정을 이용하는 의존성 주입 
 @Configuration
+// XML의 '컴포넌트 스캔' 과 같은 역할
+// 지정된 패키지 속 클래스들을 모두 스캔 후 관리 대상들을 관리한다. 
 @ComponentScan(basePackages= {"org.zerock.sample"})
 public class RootConfig {
+	// 
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		
+		hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+		hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/firstDB");
+		hikariConfig.setUsername("root");
+		hikariConfig.setPassword("12341234");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+		}
 
 }
