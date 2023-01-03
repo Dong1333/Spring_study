1389da0c76cdd01c67a746c5b689538298320183
Dong1333 <mogijisung13@gmail.com>
2023-01-03 23:44:06 +0900

    파라미터의 수집과 변환

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
 
-import jdk.internal.org.jline.utils.Log;
+import lombok.extern.log4j.Log4j;
+
 
 // 작성된 SampleController 클래스는 자동으로 스프링의 객체로 등록되게 된다.
 // @Controller 어노테이션은 클래스를 Spring MVC 컨트롤러로 표시하는데 사용
@@ -13,17 +16,34 @@ import jdk.internal.org.jline.utils.Log;
 
 // SampleController 클래스의 모든 메서드들의 기본적인 URL경로
 @RequestMapping("/sample/*")
+@Log4j
 public class SampleController {
 	
 	@RequestMapping(value = "/basic", method = {RequestMethod.GET,
 			RequestMethod.POST})
 	public void basic() {
-		Log.info("basic  get..........");
+		log.info("basic  get..........");
 	}
 	
 	@GetMapping("/basicOnlyGet")
 	public void basicGet2() {
-		Log.info("basic get only get.........");
+		log.info("basic get only get.........");
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info("" + dto);
+		
+		return "ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name,
+			@RequestParam("age") int age) {
+		log.info("name : " + name);
+		log.info("age : " + age);
+		
+		return "ex02";
 	}
 
 }
--- ex01/src/main/java/org/zerock/domain/SampleDTO.java
+++ ex01/src/main/java/org/zerock/domain/SampleDTO.java
@@ -1,5 +1,14 @@
 package org.zerock.domain;
 
+import lombok.Data;
+//SampleController의 메서드가 SampleDTO를 파라미터로 사용하게 되면
+// 자동으로 setter 메서드가 동작하면서 파라미터를 수집하게 된다.
+
+
+// getter/setter, equals(), toString() 자동 생성을 위한 어노테이션
+@Data
 public class SampleDTO {
+	private String name;
+	private int age;
 
 }
