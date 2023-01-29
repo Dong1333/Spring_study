c125cbbdca310a6cfa3f17c5585d67ffeced1372
Dong1333 <mogijisung13@gmail.com>
2023-01-30 00:52:49 +0900

    검색의 기준을 정하는 Criteria 클래스 작성

---
+++ ex02/src/main/java/org/zerock/domain/Criteria.java
@@ -0,0 +1,25 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+// 검색의 기준을 정하는 Criteria 클래스 작성
+public class Criteria {
+	// '페이지 번호', '페이지당 보여줄 글 개수' 변수 선언
+	private int pageNum;
+	private int amount;
+	
+	public Criteria() {
+		this(1,10);
+	}
+
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+
+}
